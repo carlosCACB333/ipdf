a8c10f7f9c635659ce3338ab65a3ae318c9921f5
Carlos Castillo <carloscb8080@gmail.com>
2024-07-10 16:13:35 -0500

    feat: add style to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Lock, MoodSmile, Rocket } from "@/components/icons";
 import { text } from "@/components/primitives";
 import { menus } from "@/config/menus";
 import { Card, CardBody } from "@nextui-org/card";
@@ -19,25 +20,88 @@ export default function Home() {
         </h2>
       </div>
 
-      <div className="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {menus.map((menu) => (
-          <Card key={menu.title} className="w-[300px]">
-            <CardBody>
-              <Link href={menu.href}>
+      <div>
+        <h2 className={text({ size: "lg", font: "bold" })}>
+          ¿Qué quieres hacer?
+        </h2>
+        <p className={text({ color: "disabled" })}>
+          Elige una de las opciones a continuación para comenzar a trabajar con
+          tus PDFs.
+        </p>
+        <br />
+        <div className="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {menus.map((menu) => (
+            <Card key={menu.title} isPressable as={Link} href={menu.href}>
+              <CardBody>
                 <div className="flex items-center gap-4 cursor-pointer">
-                  <menu.Icon className="text-primary" size={60}  />
+                  <menu.Icon className="text-primary" size={60} />
                   <div>
                     <h3 className={text({ font: "bold" })}>{menu.title}</h3>
-                    <p className={text({ color: "disabled", size: "xs" })}>
+                    <p className={text({ color: "disabled", size: "sm" })}>
                       {menu.description}
                     </p>
                   </div>
                 </div>
-              </Link>
+              </CardBody>
+            </Card>
+          ))}
+        </div>
+      </div>
+      <div>
+        <h2
+          className={text({
+            size: "lg",
+            font: "bold",
+          })}
+        >
+          ¡Descubre iPDF!
+        </h2>
+        <p className={text({ color: "disabled" })}>
+          Conoce algunas de las características que hacen de iPDF la mejor
+          solución para trabajar con tus PDFs.
+        </p>
+        <br />
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+          <Card shadow="sm">
+            <CardBody>
+              <Rocket size={40} />
+              <h3 className={text({ size: "md", font: "bold" })}>
+                Rápido y eficiente
+              </h3>
+              <p className={text({ color: "disabled", size: "sm" })}>
+                Trabaja con tus PDFs de manera ágil y sin complicaciones.
+              </p>
             </CardBody>
           </Card>
-        ))}
+          <Card shadow="sm">
+            <CardBody>
+              <Lock size={40} />
+              <h3 className={text({ size: "md", font: "bold" })}>
+                Seguro y confiable
+              </h3>
+              <p className={text({ color: "disabled", size: "sm" })}>
+                Tus datos están protegidos con las mejores prácticas de
+                seguridad.
+              </p>
+            </CardBody>
+          </Card>
+          <Card shadow="sm">
+            <CardBody>
+              <MoodSmile size={40} />
+              <h3 className={text({ size: "md", font: "bold" })}>
+                Fácil de usar
+              </h3>
+              <p className={text({ color: "disabled", size: "sm" })}>
+                Interfaz intuitiva y amigable para que puedas trabajar sin
+                problemas.
+              </p>
+            </CardBody>
+          </Card>
+        </div>
       </div>
+      <br />
+      <br />
+      <br />
     </section>
   );
 }
--- components/icons.tsx
+++ components/icons.tsx
@@ -454,6 +454,7 @@ export const PngFile: React.FC<IconSvgProps> = ({
     </svg>
   );
 };
+
 export const FileText: React.FC<IconSvgProps> = ({
   fill = "currentColor",
   size,
@@ -480,6 +481,90 @@ export const FileText: React.FC<IconSvgProps> = ({
       <path d="M9 9l1 0" />
       <path d="M9 13l6 0" />
       <path d="M9 17l6 0" />
+    </svg>
+  );
+};
+
+export const Rocket: React.FC<IconSvgProps> = ({
+  fill = "currentColor",
+  size,
+  height,
+  width,
+  ...props
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="None"
+      height={size || height || 24}
+      viewBox="0 0 24 24"
+      width={size || width || 24}
+      strokeWidth="1.5"
+      stroke={fill}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M4 13a8 8 0 0 1 7 7a6 6 0 0 0 3 -5a9 9 0 0 0 6 -8a3 3 0 0 0 -3 -3a9 9 0 0 0 -8 6a6 6 0 0 0 -5 3" />
+      <path d="M7 14a6 6 0 0 0 -3 6a6 6 0 0 0 6 -3" />
+      <path d="M15 9m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+    </svg>
+  );
+};
+export const Lock: React.FC<IconSvgProps> = ({
+  fill = "currentColor",
+  size,
+  height,
+  width,
+  ...props
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="None"
+      height={size || height || 24}
+      viewBox="0 0 24 24"
+      width={size || width || 24}
+      strokeWidth="1.5"
+      stroke={fill}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6z" />
+      <path d="M11 16a1 1 0 1 0 2 0a1 1 0 0 0 -2 0" />
+      <path d="M8 11v-4a4 4 0 1 1 8 0v4" />
+    </svg>
+  );
+};
+
+export const MoodSmile: React.FC<IconSvgProps> = ({
+  fill = "currentColor",
+  size,
+  height,
+  width,
+  ...props
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="None"
+      height={size || height || 24}
+      viewBox="0 0 24 24"
+      width={size || width || 24}
+      strokeWidth="1.5"
+      stroke={fill}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+      <path d="M9 10l.01 0" />
+      <path d="M15 10l.01 0" />
+      <path d="M9.5 15a3.5 3.5 0 0 0 5 0" />
     </svg>
   );
 };
